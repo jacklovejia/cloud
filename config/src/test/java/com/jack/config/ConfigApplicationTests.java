9c6aee7dba8fb7c6099d14a430bdc22886f217a2
gaohx <1109546392@qq.com>
2019-12-27 09:36:05 +0800

    config-server

---
+++ config/src/test/java/com/jack/config/ConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jack.config;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
