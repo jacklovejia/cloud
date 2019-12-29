7399105b3733783345aafddd70fec940fdbb984d
gaohx <1109546392@qq.com>
2019-12-29 10:35:29 +0800

    feign 测试

--- feign-client/src/main/java/com/jack/feignclient/config/FeignConfig.java
+++ feign-client/src/main/java/com/jack/feignclient/config/FeignConfig.java
@@ -8,6 +8,8 @@ import org.springframework.context.annotation.Configuration;
 public class FeignConfig {
     @Bean
     Retryer feignRetryer() {
+//        return new Retryer.Default(1000, SECONDS.toMillis(100), 5);
+//        return new Retryer.Default();
         return Retryer.NEVER_RETRY;
     }
 }
--- feign-client/src/main/java/com/jack/feignclient/feign/ServiceFeign.java
+++ feign-client/src/main/java/com/jack/feignclient/feign/ServiceFeign.java
@@ -1,5 +1,6 @@
 package com.jack.feignclient.feign;
 
+import com.jack.feignclient.config.FeignConfig;
 import com.jack.feignclient.feign.fallback.ServiceFeignFallbackFactory;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,7 +8,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(value = "service-feign", /*fallback = ServiceFeignFallback.class*/ fallbackFactory = ServiceFeignFallbackFactory.class)
+@FeignClient(value = "service-feign", /*fallback = ServiceFeignFallback.class*/
+        fallbackFactory = ServiceFeignFallbackFactory.class,
+        configuration = {FeignConfig.class})
 public interface ServiceFeign {
     @GetMapping(value = "/api/test01")
     String test01(@RequestParam String name);
